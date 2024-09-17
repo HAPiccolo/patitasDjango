a16c9155537173a5b4a9a27bd808e27338ba322f
HAPiccolo <hapiccolo86@gmail.com>
2024-09-17 13:39:35 -0300

    Mejora models y admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Caracteristicas, Adoptante, Empleado, Mascota, Adopciones
+
+
+class CaracteristicasAdmin(admin.ModelAdmin):
+    list_display = (
+        "edad",
+        "discapacidad",
+        "raza",
+        "sexo",
+        "especie",
+        "nombre",
+        "fecha_ingreso",
+        "foto",
+    )
+
 
 # Register your models here.
+admin.site.register(Caracteristicas, CaracteristicasAdmin)
+admin.site.register(Adoptante)
+admin.site.register(Empleado)
+admin.site.register(Mascota)
+admin.site.register(Adopciones)
--- core/models.py
+++ core/models.py
@@ -5,53 +5,53 @@ from django.db import models
 
 
 class Adoptante(models.Model):
-    id_adoptate = models.IntegerField(primary_key=True, null=False)
     nombre = models.CharField(max_length=50)
     apellido = models.CharField(max_length=50)
     telefono = models.CharField(max_length=20)
     direccion = models.CharField(max_length=200)
     edad = models.CharField(max_length=2)
-    dni = models.CharField(max_length=10)
+    dni = models.CharField(max_length=10, primary_key=True, null=False)
 
 
 class Caracteristicas(models.Model):
-    id_caracteristica = models.IntegerField(primary_key=True, null=False)
-    id_mascota = models.ForeignKey("mascotas", on_delete=models.CASCADE)
     edad = models.CharField(max_length=2)
-    discapacidad = models.CharField(max_length=500)
+    discapacidad = models.CharField(max_length=500, null=True, blank=True)
     raza = models.CharField(max_length=50)
     sexo = models.CharField(
         max_length=10,
         choices=[("macho", "Macho"), ("hembra", "Hembra")],
         default="Macho",
     )
+    especie = models.CharField(
+        max_length=50,
+        choices=[("canino", "Canino"), ("felino", "Felino")],
+        default="Canino",
+    )
     nombre = models.CharField(max_length=50)
-    fecha_ingreso = models.DateTimeField()
-    foto = models.ImageField()
+    fecha_ingreso = models.DateField()
+    foto = models.ImageField(null=True, blank=True)
 
 
 class Empleado(models.Model):
-    id_empleado = models.IntegerField(primary_key=True, null=False)
     nombre = models.CharField(max_length=50)
     apellido = models.CharField(max_length=50)
     direccion = models.CharField(max_length=200)
     telefono = models.CharField(max_length=20)
-    dni = models.CharField(max_length=10)
+    dni = models.CharField(max_length=10, primary_key=True, null=False)
 
 
-class Especie(models.Model):
-    id_especie = models.IntegerField(primary_key=True, null=False)
-    especie = models.CharField(max_length=20)
-
-
-class Mascotas(models.Model):
-    id_mascota = models.IntegerField(primary_key=True, null=False)
-    id_especie = models.ForeignKey(Especie, on_delete=models.CASCADE)
-    id_adopciones = models.ForeignKey("Adopciones", on_delete=models.CASCADE)
+class Mascota(models.Model):
+    adoptante = models.ForeignKey(Adoptante, on_delete=models.CASCADE)
+    empleado = models.ForeignKey(Empleado, on_delete=models.CASCADE)
+    masco = models.ForeignKey(Caracteristicas, on_delete=models.CASCADE)
 
 
 class Adopciones(models.Model):
-    id_adopciones = models.IntegerField(primary_key=True, null=False)
-    id_adoptante = models.ForeignKey(Adoptante, on_delete=models.CASCADE)
-    id_empleado = models.ForeignKey(Empleado, on_delete=models.CASCADE)
-    id_mascota = models.ForeignKey(Mascotas, on_delete=models.CASCADE)
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
+    telefono = models.CharField(max_length=20)
+    direccion = models.CharField(max_length=200)
+    edad = models.CharField(max_length=2)
+    dni = models.CharField(max_length=10, primary_key=True, null=False)
+    mascota = models.ForeignKey(Caracteristicas, on_delete=models.CASCADE)
+    fecha_adopcion = models.DateField()
