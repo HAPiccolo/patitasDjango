037ea77348458e99c25ec205dd19b0f05d555510
HAPiccolo <hapiccolo86@gmail.com>
2024-10-02 14:06:04 -0300

    Agrgado datos en el admin

--- core/views.py
+++ core/views.py
@@ -23,4 +23,23 @@ def login(request):
 
 @login_required()
 def administracion(request):
-    return render(request, "core/admin.html")
+    cantMascotas = Mascotas.objects.all().count()
+    caninos = Mascotas.objects.filter(especie="canino").count()
+    canMacho = Mascotas.objects.filter(especie="canino", sexo="macho").count()
+    canHembra = Mascotas.objects.filter(especie="canino", sexo="hembra").count()
+
+    felinos = Mascotas.objects.filter(especie="felino").count()
+    felMacho = Mascotas.objects.filter(especie="felino", sexo="macho").count()
+    felHembra = Mascotas.objects.filter(especie="felino", sexo="hembra").count()
+
+    context = {
+        "cantMascotas": cantMascotas,
+        "caninos": caninos,
+        "felinos": felinos,
+        "canMacho": canMacho,
+        "canHembra": canHembra,
+        "felMacho": felMacho,
+        "felHembra": felHembra,
+    }
+
+    return render(request, "core/admin.html", context)
